1d64d45669d1af9b3e542e71ef5301a9f333b456
zklosko <31484611+zklosko@users.noreply.github.com>
2026-08-19 21:56:15 -0400

    wrote first test

---
+++ EchoServer.Tests/Tests/State.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using EchoServer.State;
+
+namespace EchoServer.Tests
+{
+    public class TestState
+    {
+        /// <summary>
+        /// Create state instance unique to each test
+        /// </summary>
+        /// <returns>Blank State class</returns>
+        private State.State CreateState()
+        {
+            return new State.State("\r", new List<Subscriber>());
+        }
+
+        [Fact]
+        public void Test_AllSpacesOffInNewZone()
+        {
+            // Arrange - set up all things test needs
+            var state = CreateState();
+
+            // Act - perform test
+            var IsOff = true;
+            for (int s = 1; s <= 16; s++)
+            {
+                if (!state.IsSpaceOff(s))
+                {
+                    IsOff = false;
+                    break;
+                }
+            }
+
+            // Assert - verify outcome of test
+            Assert.True(IsOff);
+        }
+    }
+}
